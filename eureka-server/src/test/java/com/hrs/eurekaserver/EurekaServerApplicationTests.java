1f66da9c54c9c0c92a6a994622c7566dfa3ee930
Saurav <sauravv30@gmail.com>
2024-01-22 18:59:02 +0530

    First commit

---
+++ eureka-server/src/test/java/com/hrs/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,19 @@
+package com.hrs.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * The type Eureka server application tests.
+ */
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    /**
+     * Context loads.
+     */
+    @Test
+	void contextLoads() {
+	}
+
+}
